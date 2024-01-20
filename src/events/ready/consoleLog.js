bcef059024258191b19dac874b8636e9e88017b2
Sadiq Shahid <sadiq.shahid101@gmail.com>
2024-01-20 16:59:08 -0400

    Set custom status

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,3 +1,14 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
+	
+	client.user.setPresence({
+		activities: [{
+			type: ActivityType.Watching,
+			name: "www.francheye.com",
+			url: 'www.francheye.com'
+		}],
+		status: "online"
+	})
 };
